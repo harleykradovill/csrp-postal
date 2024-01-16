635bce7515db40b554cfde66b050e99ad2c5b0c7
Harley Kradovill <43226649+harleykradovill@users.noreply.github.com>
2024-01-16 15:14:06 -0600

    filenames

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+-- Client Scripts
+client_script 'postal_client.lua'
+
+-- Server Scripts
+server_script 'postal_server.lua'
+
+-- Other Files
+file 'postals.json'
